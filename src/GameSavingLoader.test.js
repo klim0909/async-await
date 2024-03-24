e24aaf11262dfe19004331ff07c44d5519a669d3
klim <klim.volchenkov.04@bk.ru>
2024-03-25 00:02:35 +0300

    new await

--- src/GameSavingLoader.test.js
+++ src/GameSavingLoader.test.js
@@ -1,18 +1,17 @@
-import GameSavingLoader from './GameSavingLoader.js';
+import GameSavingLoader from './app.js'; // Импортируем класс GameSavingLoader из файла app.js
 
 describe('GameSavingLoader', () => {
-  test('should load game saving data', () => {
-    return GameSavingLoader.load().then((saving) => {
-      expect(saving).toEqual({
-        "id": 9,
-        "created": 1546300800,
-        "userInfo": {
-          "id": 1,
-          "name": "Hitman",
-          "level": 10,
-          "points": 2000
-        }
-      });
+  test('load', async () => {
+    const saving = await GameSavingLoader.load();
+    expect(saving).toEqual({
+      id: 9,
+      created: 1546300800,
+      userInfo: {
+        id: 1,
+        name: 'Hitman',
+        level: 10,
+        points: 2000
+      }
     });
   });
 });
--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
-import GameSavingLoader from './GameSavingLoader.js';
+import read from './reader.js';
+import json from './parser.js';
 
-GameSavingLoader.load().then((saving) => {
-  console.log("Saving loaded:", saving);
-}, (error) => {
-  console.error("Error loading saving:", error);
-});
+class GameSavingLoader {
+  static async load() {
+    try {
+      const data = await read(); // Чтение данных из файла
+      const value = await json(data); // Преобразование данных в JSON
+      return JSON.parse(value); // Возвращаем объект GameSaving
+    } catch (error) {
+      throw new Error(`Failed to load game saving: ${error}`);
+    }
+  }
+}
+
+export default GameSavingLoader; // Экспортируем класс GameSavingLoader из файла app.js
